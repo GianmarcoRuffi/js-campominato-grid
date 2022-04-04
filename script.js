8b840e0216277c3b74e9c3235b942caee14fb6be
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-04-04 17:59:47 +0200

    randomizzata array campo di gioco

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ function createBoard() {
   console.log(emptyArray);
   const gameArray = emptyArray.concat(bombsArray);
   console.log(gameArray);
+  const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
+  console.log(shuffledArray);
   for (let i = 0; i < width * width; i++) {
     const square = document.createElement("div");
     square.setAttribute("id", i);
