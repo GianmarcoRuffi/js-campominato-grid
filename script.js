fcceea3192c99b7d369785238846bcd98dcdc2ac
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-04-04 17:34:31 +0200

    reimpostazione css

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro. */
+
+// Assegnazione variabili elementi e array
+
+const grid = document.querySelector(".grid");
+let width = 10;
+let squares = [];
+
+// Creazione Tabella
+
+function createBoard() {
+  for (let i = 0; i < width * width; i++) {
+    const square = document.createElement("div");
+    square.setAttribute("id", i);
+    grid.appendChild(square);
+    squares.push(square);
+  }
+}
+
+createBoard();
